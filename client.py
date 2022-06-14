b3851c0f109058c4ea3b9f9872f319ce4d17d3de
lewisLANG <39106969+lewisLANG@users.noreply.github.com>
2022-06-14 13:54:14 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+-*- coding: utf-8 -*-
+@Author: XF010101
+@SoftWare: PyCharm
+@File: ssaskclient.py
+@Time: 2022/6/13 12:16
+"""
 import socket
 import threading
+
+
+def encodeL(text):
+    # 加密
+    text2 = ''  # 用来存放加密之后的内容
+    i = 0
+    # 用循环是为了替换掉文本中的每一个字符
+    while i < len(text):
+        # ord(text[i]) 意为先将字符转为ASCII码
+        # chr(...) 意为将处理好的ASCII码转换成字符
+        text2 = text2 + chr(ord(text[i]) + 1)
+        i = i + 1
+    return text2
+
+
+def decodeL(text2):
+    text3 = ''  # 用来存放解密之后的内容
+    i = 0
+    # 用循环是为了替换掉文本中的每一个字符
+    while i < len(text2):
+        # ord(text2[i]) 意为先将字符转为ASCII码
+        # chr(...) 意为将处理好的ASCII码转换成字符
+        text3 = text3 + chr(ord(text2[i]) - 1)
+        i = i + 1
+    return text3
 
 
 def recv(sock, addr):
@@ -8,10 +41,14 @@ def recv(sock, addr):
     也就是需要send一次，否则win下会报错
     '''
     sock.sendto(name.encode('utf-8'), addr)
-    #print(name, addr)
     while True:
         data = sock.recv(1024)
-        print(data.decode('utf-8'))
+        datadecode = data.decode('utf-8')
+        if " : " in datadecode:
+            datastr = datadecode.split(' : ')[0] + " : " + decodeL(datadecode.split(' : ')[1])
+        else:
+            datastr = datadecode
+        print(datastr)
 
 
 def send(sock, addr):
@@ -23,7 +60,7 @@ def send(sock, addr):
     '''
     while True:
         string = input('')
-        message = name + ' : ' + string
+        message = name + ' : ' + encodeL(string)
         data = message.encode('utf-8')
         sock.sendto(data, addr)
         if string.lower() == 'EXIT'.lower():
@@ -35,7 +72,7 @@ def main():
         主函数执行方法，通过多线程来实现多个客户端之间的通信
     '''
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server = ('x.x.x.x', 9999)
+    server = ('x.x.x.x', 7878)
     tr = threading.Thread(target=recv, args=(s, server), daemon=True)
     ts = threading.Thread(target=send, args=(s, server))
     tr.start()
